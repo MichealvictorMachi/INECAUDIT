b29278548c7b92bec07e9c911eec56ce72c1a1c1
Victor Micheal <michealvictor175@gmail.com>
2022-08-10 09:02:48 +0100

    Add project files.

---
+++ INECAUDIT/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace INECAUDIT
+{
+    internal class InecDataBase
+    {
+        static void Main(string[] args)
+        {
+            string[] RegVoters = { "Success", "Great", "Peter", "Favour", "Mike", "Success", "Loveth", "Joseph", "Peter", "Victor", "Godwin", "Jeff", "Success", "Olatunde", "Bolaji", "Peter", "Great" };
+            Dictionary<string, int> Database = new Dictionary<string, int>();
+            foreach (var item in RegVoters)
+            {
+                if (Database.ContainsKey(item)) 
+                { 
+                    Database[item] = Database[item] + 1;
+                }
+                else
+                {
+                    Database[item] = 1;
+                }
+            }
+            Console.WriteLine("Names and number of times registered");
+            foreach (var item in Database)
+            {
+                if(item.Value > 1)
+                {
+                    Console.WriteLine(item.Key + ":" + item.Value);
+                }
+            }
+
+            // INEC SHOULD USE THE HASHSET CONCEPT TO ENSURE DUPLICATES ARE REMOVED AND ENSURE DISTINCTIVENESS.
+            Console.WriteLine("\n\nThe Distinctive List");
+            HashSet<string> Distinctive = RegVoters.ToHashSet();
+            foreach(var item in Distinctive)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
